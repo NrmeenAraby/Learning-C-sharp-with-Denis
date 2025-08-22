2c7f98fb1cfe1837327f8fb7050c9375ee070e77
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-08-23 02:15:10 +0300

    Generic Interface

---
+++ oldSchool/IRepositoryT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oldSchool
+{
+    internal interface IRepositoryT<T>
+    {
+        void Add (T entity);
+        void Remove(T entity);
+    }
+
+    internal class productRepository : IRepositoryT<Product>
+    {
+        public void Add (Product entity)
+        {
+
+        }
+        public void Remove(Product entity)
+        {
+
+        }
+    }
+}
